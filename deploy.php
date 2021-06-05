ea2ed11724f16d142a6f9f953e031a7990124e9d
Damar Albaribin <damaralbaribin@gmail.com>
2021-06-05 10:17:49 +0700

    fix : deployment issue

--- deploy.php
+++ deploy.php
@@ -3,60 +3,45 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
-require 'recipe/rsync.php';
 
-set('bin/composer', function () {
-    return '/usr/bin/php7.4 /usr/bin/composer';
-});
+// Project name
+set('application', 'my_project');
 
-set('application', 'Academy');
-set('ssh_multiplexing', true);
+// Project repository
+set('repository', 'https://github.com/damarsimple/academy-app.git');
 
-set('rsync_src', function () {
-    return __DIR__;
-});
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
 
 
-add('rsync', [
-    'exclude' => [
-        '.git',
-        '/.env',
-        '/storage/',
-        '/vendor/',
-        '/node_modules/',
-        '.github',
-        'deploy.php',
-    ],
-]);
-
-task('deploy:secrets', function () {
-    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
-    upload('.env', get('deploy_path') . '/shared');
-});
+// Hosts
 
 host('api.dslab.id')
-    ->hostname('47.241.128.75')
-    ->stage('production')
-    ->user('root')
+    ->setRemoteUser('root')
     ->set('deploy_path', '/var/www/academy-be');
 
-desc('Deploy the application');
+// Tasks
 
-task('deploy', [
-    'deploy:info',
-    'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'rsync',
-    'deploy:secrets',
-    'deploy:shared',
-    'deploy:vendors',
-    'deploy:writable',
-    'artisan:storage:link',
-    'artisan:view:cache',
-    'artisan:config:cache',
-    'artisan:migrate',
-    'artisan:queue:restart',
-    'deploy:symlink',
-    'cleanup',
-]);
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate:fresh');
+
+task('data:fake', function () {
+    run('cd {{release_path}} && php artisan data:fake');
+});
+
+after('artisan:migrate:fresh', 'data:fake');
